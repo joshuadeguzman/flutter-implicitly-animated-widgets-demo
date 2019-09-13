ec3a4e1f652e73b1697b4e46fb20920a8f3d18a3
jdeguzman <devjoshuadeguzman@gmail.com>
2019-09-14 03:19:57 +0800

    Add animated padding

--- lib/screens/animated_bar_charts.dart
+++ lib/screens/animated_bar_charts.dart
@@ -19,12 +19,15 @@ class AnimatedBarChartsScreen extends StatefulWidget {
 class AnimatedBarChartsScreenState extends State<AnimatedBarChartsScreen> {
   String get _widgetTitle => AnimatedBarChartsScreen.SCREEN_TITLE;
   bool _isLoadingBarChart = true;
-  double _barChart1Height = 150;
-  double _barChart2Height = 150;
-  double _barChart3Height = 100;
+  double _barChart1Height = 90;
+  double _barChart2Height = 170;
+  double _barChart3Height = 300;
   Color _barChart1Color = Colors.blue;
   Color _barChart2Color = Colors.orange;
   Color _barChart3Color = Colors.green;
+  double _barChart1PaddingTop = 60;
+  double _barChart2PaddingTop = 20;
+  double _barChart3PaddingTop = 240;
   TextStyle _barChartLabel = TextStyle(color: Colors.black54, fontSize: 10);
 
   @override
@@ -65,7 +68,7 @@ class AnimatedBarChartsScreenState extends State<AnimatedBarChartsScreen> {
                       setState(() {
                         _barChart1Height = 270;
                         _barChart2Height = 350;
-                        _barChart3Height = 40;
+                        _barChart3Height = 60;
 
                         _barChart1Color = Colors.blueAccent;
                         _barChart2Color = Colors.orangeAccent;
@@ -76,6 +79,10 @@ class AnimatedBarChartsScreenState extends State<AnimatedBarChartsScreen> {
                           fontSize: 16,
                           fontWeight: FontWeight.bold,
                         );
+
+                        _barChart1PaddingTop = 140;
+                        _barChart2PaddingTop = 250;
+                        _barChart3PaddingTop = 20;
 
                         _isLoadingBarChart = false;
                       });
@@ -96,11 +103,14 @@ class AnimatedBarChartsScreenState extends State<AnimatedBarChartsScreen> {
                         _barChart2Color = Colors.orange;
                         _barChart3Color = Colors.green;
 
+                        _barChart1PaddingTop = 60;
+                        _barChart2PaddingTop = 20;
+                        _barChart3PaddingTop = 240;
+
                         _barChartLabel = TextStyle(
-                          color: Colors.white,
-                          fontSize: 10,
-                          fontWeight: FontWeight.normal
-                        );
+                            color: Colors.white,
+                            fontSize: 10,
+                            fontWeight: FontWeight.normal);
 
                         _isLoadingBarChart = false;
                       });
@@ -136,18 +146,23 @@ class AnimatedBarChartsScreenState extends State<AnimatedBarChartsScreen> {
             height: _barChart1Height,
             width: 75,
             color: _barChart1Color,
-            child: Container(
-              padding: EdgeInsets.only(bottom: 10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  AnimatedDefaultTextStyle(
-                    curve: Curves.bounceInOut,
-                    duration: Duration(milliseconds: 500),
-                    child: Text('Blue'),
-                    style: _barChartLabel,
-                  ),
-                ],
+            child: AnimatedPadding(
+              duration: Duration(milliseconds: 500),
+              padding: EdgeInsets.only(top: _barChart1PaddingTop),
+              child: Container(
+                color: Colors.black12,
+                padding: EdgeInsets.only(bottom: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    AnimatedDefaultTextStyle(
+                      curve: Curves.bounceInOut,
+                      duration: Duration(milliseconds: 500),
+                      child: Text('Blue'),
+                      style: _barChartLabel,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -157,18 +172,23 @@ class AnimatedBarChartsScreenState extends State<AnimatedBarChartsScreen> {
             height: _barChart2Height,
             width: 75,
             color: _barChart2Color,
-            child: Container(
-              padding: EdgeInsets.only(bottom: 10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  AnimatedDefaultTextStyle(
-                    curve: Curves.bounceInOut,
-                    duration: Duration(milliseconds: 500),
-                    child: Text('Orange'),
-                    style: _barChartLabel,
-                  ),
-                ],
+            child: AnimatedPadding(
+              duration: Duration(milliseconds: 500),
+              padding: EdgeInsets.only(top: _barChart2PaddingTop),
+              child: Container(
+                color: Colors.black12,
+                padding: EdgeInsets.only(bottom: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    AnimatedDefaultTextStyle(
+                      curve: Curves.bounceInOut,
+                      duration: Duration(milliseconds: 500),
+                      child: Text('Orange'),
+                      style: _barChartLabel,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -178,18 +198,23 @@ class AnimatedBarChartsScreenState extends State<AnimatedBarChartsScreen> {
             height: _barChart3Height,
             width: 75,
             color: _barChart3Color,
-            child: Container(
-              padding: EdgeInsets.only(bottom: 10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  AnimatedDefaultTextStyle(
-                    curve: Curves.bounceInOut,
-                    duration: Duration(milliseconds: 500),
-                    child: Text('Green'),
-                    style: _barChartLabel,
-                  ),
-                ],
+            child: AnimatedPadding(
+              duration: Duration(milliseconds: 500),
+              padding: EdgeInsets.only(top: _barChart3PaddingTop),
+              child: Container(
+                color: Colors.black12,
+                padding: EdgeInsets.only(bottom: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    AnimatedDefaultTextStyle(
+                      curve: Curves.bounceInOut,
+                      duration: Duration(milliseconds: 500),
+                      child: Text('Green'),
+                      style: _barChartLabel,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
